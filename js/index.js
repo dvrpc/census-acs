26f9f7dde13ee3cd15ffe9250906df58acc03fa9
mmolta <mcmolta10@gmail.com>
2021-12-28 17:28:34 -0500

    begin setting up form toggle events

--- js/index.js
+++ js/index.js
@@ -1,18 +1,27 @@
 import makeMap from './map.js'
 import sources from './mapSources.js'
 import mapLayers from './mapLayers.js'
+import secondaryLayers from './secondaryMapLayers.js'
 import { makePopup, makePopupContent } from './popup.js'
 // add additional imports here (popups, forms, etc)
 
 
 
-const form = document.getElementById('nav-form')
+const toggleForm = document.getElementById('toggle-form')
 // get additional elements here (forms, etc)
 
 
+
 // variables
-const hoverLayers = ['municipality-fill', 'county-fill']
-const urlPrefix = 'https://data.census.gov/cedsci/profile?g='
+const urlCodes = {
+    municipality: '0600000US',
+    county: '0500000US',
+    tract: '1400000US',
+    block: '1000000US',
+    'block groups': '1500000US'
+}
+
+
 
 // map
 const map = makeMap()
@@ -23,27 +32,62 @@ map.on('load', () => {
 
     const popup = makePopup()
 
-    map.on('mousemove', 'municipality-fill', e => {
+    // default events
+    setHoverEvents('municipality', map, popup)
+
+    toggleForm.onchange = e => {
+        const options = Array.from(e.target.children)
+
+        options.forEach(option => {
+            const val = option.value
+
+            if(option.selected) setHoverEvents(val, map, popup)
+            else removeHoverEvents(val, map)
+        })
+    }
+})
+
+const setHoverEvents = (val, map, popup) => {
+    const code = urlCodes[val]
+
+    // add secondary layers on first pass
+    if(!map.getLayer(`${val}-outline`)) {
+        addHoverLayers(val, map)
+    }
+
+    map.on('mousemove', `${val}-fill`, e => {
         map.getCanvas().style.cursor = 'pointer'
-        map.setFilter('municipality-hover', ['==', 'geoid', e.features[0].properties['geoid']])
+        map.setFilter(`${val}-hover`, ['==', 'geoid', e.features[0].properties['geoid']])
 
         makePopupContent(map, e, popup)
     })
 
-    map.on('mouseleave', 'municipality-fill', e =>{
+    map.on('mouseleave', `${val}-fill`, e =>{
         map.getCanvas().style.cursor = ''
-        map.setFilter('municipality-hover', ['==', 'geoid', ''])
+        map.setFilter(`${val}-hover`, ['==', 'geoid', ''])
 
         popup.remove()
     })
 
-    map.on('click', 'municipality-fill', e => {
-        const code = '0600000US'
+    map.on('click', `${val}-fill`, e => {
         const geoid = e.features[0].properties['geoid']
         window.open(`https://data.census.gov/cedsci/profile?g=${code}${geoid}`)
     })
+}
 
-    navForm.onchange = e => {
-        e.target
-    }
-})
+const addHoverLayers = (val, map) => {
+    secondaryLayers[`${val}Outline`]
+
+    map.addLayer(secondaryLayers[`${val}Outline`])
+    map.addLayer(secondaryLayers[`${val}Hover`])
+    map.addLayer(secondaryLayers[`${val}Fill`])
+}
+
+const removeHoverEvents = (val, map) => {
+
+    if(!map.getLayer(`${val}-outline`)) return
+
+    map.setLayoutProperty(`${val}-outline`, 'visibility', 'none')
+    map.setLayoutProperty(`${val}-fill`, 'visibility', 'none')
+    map.setLayoutProperty(`${val}-hover`, 'visibility', 'none')
+}
